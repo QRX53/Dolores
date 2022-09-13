681de54a0f580e138a013ae69cb2e79eb1951a7e
primevibetime <69993408+44-million@users.noreply.github.com>
2022-09-13 15:26:23 -0600

    W (not L)

---
+++ app/src/main/java/com/example/dolores/MaybeSettings.java
@@ -0,0 +1,70 @@
+package com.example.dolores;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.dolores.databinding.ActivityMainBinding;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link MaybeSettings#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class MaybeSettings extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    public MaybeSettings() {
+        // Required empty public constructor
+    }
+
+    ActivityMainBinding binding;
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment MaybeSettings.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static MaybeSettings newInstance(String param1, String param2) {
+        MaybeSettings fragment = new MaybeSettings();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+
+        binding = ActivityMainBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+
+    }
+
+    @Override
+    public void onViewCreated(View v, Bundle b) {
+
+
+
+
+    }
+
+}
