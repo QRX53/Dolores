a92e287af0f1fea003c8f266182388894aa29b94
primevibetime <69993408+44-million@users.noreply.github.com>
2022-10-25 15:00:50 -0600

    Update

--- app/src/main/java/com/example/dolores/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/dolores/ui/home/HomeFragment.java
@@ -5,11 +5,15 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import com.example.dolores.bosque.data.Data;
 import com.example.dolores.databinding.FragmentHomeBinding;
+
+import java.util.Objects;
 
 public class HomeFragment extends Fragment {
 
@@ -21,6 +25,37 @@ public class HomeFragment extends Fragment {
 
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+
+        binding.textView7.setText(Data.current.getUsername());
+        binding.textView8.setText(Data.current.getId());
+
+        String rating = Data.current.getGrades().getGpa();
+
+        double rating2 = Double.parseDouble(rating);
+
+        if (rating2 < 2.0) {
+            binding.ratingBar.setRating(1);
+        } else if (rating2 < 2.5 && rating2 > 2) {
+            binding.ratingBar.setRating(2);
+        } else if (rating2 < 3.5 && rating2 > 2) {
+            binding.ratingBar.setRating(3);
+        } else if (rating2 < 4.0 && rating2 > 3.5) {
+            binding.ratingBar.setRating(4);
+        } else {
+            binding.ratingBar.setRating(5);
+        }
+
+        binding.floatingActionButton2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (getContext() != null && getContext().getApplicationContext() != null) {
+                    Toast.makeText(
+                            getContext().getApplicationContext(),
+                            "Coming Soon!",
+                            Toast.LENGTH_LONG).show();
+                }
+            }
+        });
 
         return root;
     }
